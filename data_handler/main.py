900847c50b88f4770789eae60cdf36ec7eece799
Phay Vong <vongphay@gmail.com>
2018-07-14 19:54:12 -0700

    added header format to main

---
+++ data_handler/main.py
@@ -0,0 +1,13 @@
+"""
+Calculate how many of each risk score show up for any particular month
+
+
+Author: Phay Vong
+Version 1.0
+
+"""
+
+__author__ = "Phay Vong"
+__version__ = "1.0"
+
+from modules.phay_mods import *
