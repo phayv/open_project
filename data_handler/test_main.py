ae9889471e8e2cf60329b5ef47ff3e3a87025e08
Phay Vong <vongphay@gmail.com>
2018-07-18 23:27:13 -0700

    add test for filter inspection type

--- data_handler/test_main.py
+++ data_handler/test_main.py
@@ -7,3 +7,16 @@ def test_read_file():
 
 	assert len(data) == 100
 	assert len(data[0].keys()) == 17
+
+def test_filter_inspection_type():
+	data = [
+		{'inspection_type' : 'A'},
+		{'inspection_type' : 'B'},
+		{'inspection_type' : 'C'},
+		{'inspection_type' : 'A'}
+	]
+
+	assert filter_inspection_type(data, 'A') == [
+		{'inspection_type' : 'A'}, {'inspection_type' : 'A'}]
+	assert filter_inspection_type(data, 'B') == [
+		{'inspection_type' : 'B'}]
