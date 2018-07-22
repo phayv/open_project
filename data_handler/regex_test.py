76f7db1dd48232a9fb1d4311d061b9aca8685179
Phay Vong <vongphay@gmail.com>
2018-07-22 13:59:33 -0700

    adding more variations

--- data_handler/regex_test.py
+++ data_handler/regex_test.py
@@ -8,9 +8,8 @@ Return more than just the abbreviation of months
 [a-z]* because May doesn't necesarrily have anything after
 2 - 4 digits for the year
 """
-
-print(re.findall(r'\d{,2}[/-]\d{,2}[/-]\d{2,4}', datestr))
-print(re.findall(r'(?:\d{,2} )?(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[a-z]* (?:\d{,2}, )?\d{2,4}', datestr))
+part1 = r'\d{1,2}[/-]\d{1,2}[/-]\d{2,4}'
+part2 = r'(?:\d{1,2} )?(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[a-z]* (?:\d{1,2}, )?\d{2,4}'
 
 # working on combining
 """
@@ -20,6 +19,14 @@ potential Month spelled out or abbreviated followed by a space.
 potential digit(s) with comma after potential Month spelled out
 2 - 4 digits for the year
 """
-combine = r'(?:\d{,2}[ /-])?(?:\d{,2}[/-])?(?:(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[a-z]* )?(?:\d{,2}[,/-](?: )?)?\d{2,4}'
+combine = r'(?:\d{1,2}[ /-])?(?:\d{1,2}[/-])?(?:(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[a-z]* )?(?:\d{1,2}[,/-](?: )?)?\d{2,4}'
+
+# Adding more variations
+datestr2 = '2002-2-11\n2002-11-2\n2-2002-11\n11-2002-2\n2002 Jan 2\n2002 October 23\n'
+datestr = datestr + datestr2
+
+combine = r'(?:\d{1,4}[ /-])?(?:\d{1,4}[ /-])?(?:(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[a-z ]*)?(?:\d{1,4}[,/-](?: )?)?\d{1,4}'
 
 print(re.findall(combine, datestr))
+
+# there are more obscure variations we haven't tested
