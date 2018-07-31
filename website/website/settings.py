6b5b104867e6f7cbbbde376d4aed9887004aa917
phayrus <vongphay@gmail.com>
2018-07-30 21:39:07 -0700

    switching database from sqlite3 to postgresql

--- website/website/settings.py
+++ website/website/settings.py
@@ -73,12 +73,18 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'website_db',
+        'USER': 'phayrus',
+        'PASSWORD': 'simple',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
