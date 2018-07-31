a598e9dc9f41caaa1909bb5f98a473df529f76af
phayrus <vongphay@gmail.com>
2018-07-31 13:59:54 -0700

    adding blog app to the website

--- website/website/settings.py
+++ website/website/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'homepage.apps.HomepageConfig',
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
