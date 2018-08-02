c43a185e2fc53256fb4d7b5ba8183e9cf5beced9
phayrus <vongphay@gmail.com>
2018-08-01 20:14:33 -0700

    adding login and logout forms

--- website/about/urls.py
+++ website/about/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include, re_path
+from django.contrib.auth.views import login, logout
 from . import views
 
 urlpatterns = [
     path(r'', views.IndexView.as_view(), name="index"),
+    path(r'login/', login, {'template_name': 'about/login.html'}),
+    path(r'logout/', logout, {'template_name': 'about/logout.html'}),
 ]
