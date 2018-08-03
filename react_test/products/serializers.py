f5dbcac63ce4beb038fd51dd5ea9a540147f2d24
phayrus <vongphay@gmail.com>
2018-08-03 13:17:14 -0700

    finished serialization to json for react

--- react_test/products/serializers.py
+++ react_test/products/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Product
 
 
-class BandSerializer(serializers.ModelSerializer):
+class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
+        fields = '__all__'
--- react_test/products/views.py
+++ react_test/products/views.py
@@ -1,4 +1,6 @@
 from django.views import generic
+from rest_framework import generics
+from .serializers import ProductSerializer
 from .models import Product
 
 
@@ -22,3 +24,9 @@ class ProductDetailView(generic.detail.DetailView):
         context = super().get_context_data(**kwargs)
         context['object'] = Product.objects.get(id=self.kwargs['pk'])
         return context
+
+
+class ProductAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
