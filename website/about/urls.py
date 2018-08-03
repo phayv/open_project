a6e9693f4943ce7b5ab8123c030237328ea3beae
phayrus <vongphay@gmail.com>
2018-08-02 23:47:16 -0700

    condense custom css

--- website/about/urls.py
+++ website/about/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include, re_path
 from django.contrib.auth.views import login, logout
 from . import views
 
+app_name = 'about'
+
 urlpatterns = [
     path(r'', views.IndexView.as_view(), name="index"),
     path(r'login/', login, {'template_name': 'about/login.html'}),
